506286d9b008f83164b0f64327b6fa48e6ada2ce
Pedro Lopez <peanlolo@hotmail.com>
2024-03-12 22:48:07 +0100

    Añadir clases

---
+++ Practica3/FranquiciasUCCommon/src/main/java/Empleado.java
@@ -0,0 +1,154 @@
+
+
+
+import java.time.LocalDate;
+/**
+ * Clase que representa un empleado de la franquicia, 
+ * con sus datos personales 
+ * y su estado en la franquicia (baja y categoria)
+ */
+public class Empleado {
+	
+	private String DNI;
+	private String nombre;
+	private Categoria categoria;
+	private LocalDate fechaContratacion;
+	private boolean baja = false;
+	
+	public Empleado() {	}
+	
+	/**
+	 * Constructor del empleado con DNI, nombre, categoria y fecha de contratacion.
+	 * Por defecto, baja se inicializa a false. 
+	 * @param DNI
+	 * @param nombre
+	 * @param categoria
+	 * @param fechaContratacion
+	 */
+	public Empleado(String DNI, String nombre, Categoria categoria, LocalDate fechaContratacion) {
+		this.nombre = nombre;
+		this.DNI=DNI;
+		this.categoria=categoria;
+		this.fechaContratacion=fechaContratacion;
+	}
+	
+	/**
+	 * Retorna el sueldo bruto del empleado
+	 */
+	public double sueldoBruto() {
+
+		int sueldoBase = 0;
+		int complemento = 0;
+		double sueldoBruto = 0;
+
+		switch(categoria){
+			case ENCARGADO:
+				sueldoBase = 2000;
+				break;
+			case VENDEDOR:
+				sueldoBase = 1500;
+				break;
+			case AUXILIAR:
+				sueldoBase = 1000;
+				break;
+		}
+
+		//Calculamos el complemento por antiguedad
+		LocalDate hoy = LocalDate.now();
+
+		if (fechaContratacion.plusYears(20).isBefore(hoy)) {
+			complemento = 200;
+		} else if (fechaContratacion.plusYears(10).isBefore(hoy)) {
+			complemento = 100;
+		} else if (fechaContratacion.plusYears(5).isBefore(hoy)){
+			complemento = 50;
+		}
+
+		sueldoBruto = sueldoBase + complemento;
+
+		if (baja) {
+			sueldoBruto *= 0.75;
+		}
+
+		return sueldoBruto;
+
+	}
+	
+	/** 
+	 * Dar de baja al empleado
+	 */
+	public void darDeBaja() {
+		this.baja=true;
+	}
+	
+	/**
+	 * Dar de alta al empleado
+	 */
+	public void darDeAlta() {
+		this.baja=false;
+	}
+	
+	
+	/**
+	 * Retorna el dni del vendedor
+	 * @return id
+	 */
+	public String getDNI() {
+		return DNI;
+	}
+	
+	/**
+	 * Retorna el nombre del vendedor
+	 * @return nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	
+	/**
+	 * Retorna la categoria del empleado
+	 *  @return categoria
+	 */
+	public Categoria getCategoria () {
+		return categoria;
+	}
+	
+	/**
+	 * Retorna la fecha de contrato
+	 * @return Fecha de contratacion
+	 */
+	public LocalDate getFechaContratacion() {
+		return fechaContratacion;
+	}
+	
+	/**
+	 * Retorna si el empleado está de baja
+	 * @return true si esta de baja
+	 *         false si no lo esta
+	 */
+	public boolean getBaja() {
+		return baja;
+	}
+		
+	
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setFechaContratacion(LocalDate fechaContratacion) {
+		this.fechaContratacion = fechaContratacion;
+	}
+	
+	public void setBaja(boolean baja) {
+		this.baja = baja;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
